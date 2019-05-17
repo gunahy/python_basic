d4373b8702c730d4fd5f72ac861eea5e694ac85e
gunahy <freshdv@mail.ru>
2019-05-17 18:38:57 +0500

    lesson 7 мало что успел, но хотя бы это сдам

---
+++ Lesson 7/home_work/lotteron.py
@@ -0,0 +1,13 @@
+import random
+
+class Lotteron:
+    
+    def __init__(self):
+        self.barrels = [_ for _ in range(1, 90)]
+
+    def generate_number(self):
+        yield self.barrels
+
+
+
+
